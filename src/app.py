fdea5c5bc2bfd6385bcab93b7c13eafe7b45fcb3
GeaziAntunes <geaziac@hotmail.com>
2021-11-08 20:16:27 -0300

    feat: add help message for each bot command

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ from os import getenv
 # load enviroment variables from .env file
 load_dotenv()
 
-# get discord token from enviroment system
+# get discord token from enviroment
 DISCORD_TOKEN = getenv("DISCORD_TOKEN")
 
 # set discord client with commands prefix
@@ -22,12 +22,12 @@ async def on_ready():
     print(f"\a*We are already online, connected as {client.user.name}!*")
 
 
-@client.command()
+@client.command(help="Check if bot is on.")
 async def ping(ctx):
     await ctx.send("PONG!")
 
 
-@client.command(name="d")
+@client.command(name="d", help="Roll a quantity of ring and skill dice. Example: *.d 3 3*, roll three ring dice plus three skill dice.")
 async def dice_roll(ctx, ring, skill):
     dice = CustomDice(ring=ring, skill=skill)
     result = dice.format(**dice.roll())
@@ -35,7 +35,7 @@ async def dice_roll(ctx, ring, skill):
     await ctx.send(result)
 
 
-@client.command(name="r")
+@client.command(name="r", help="Roll a quantity of ring dice only. Example: *.r 3*, roll three ring dice.")
 async def ring_roll(ctx, ring):
     dice = CustomDice(ring=ring, skill=0)
     result = dice.format(**dice.roll())
@@ -44,13 +44,20 @@ async def ring_roll(ctx, ring):
     await ctx.send(result)
 
 
-@client.command(name="s")
+@client.command(name="s", help="Roll a quantity of skill dice only. Example: *.s 3*, roll 3 skill dice.")
 async def skill_roll(ctx, skill):
     dice = CustomDice(ring=0, skill=skill)
     result = dice.format(**dice.roll())
     result = result.splitlines()[1]
 
     await ctx.send(result)
+
+
+@client.command(name="t", help="Show table of emojis meanings.")
+async def table(ctx):
+    table = "👏 > Success;\n🎆 > Explosive Success;\n😫 > Strife;\n🤔 > Opportunity;\n◻ > Blank Face;"
+
+    await ctx.send(table)
 
 
 ##### MAIN #####
