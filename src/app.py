d89c27d9e8b69cd7cda87ea6eeb675e54680440a
GeaziAntunes <geaziac@hotmail.com>
2021-11-08 18:55:19 -0300

    change: change customdice functions to a class

--- src/app.py
+++ src/app.py
@@ -1,22 +1,22 @@
-import os
-import models.customdice as customdice
+from models.customdice import CustomDice
 from dotenv import load_dotenv
 from discord.ext import commands
+from os import getenv
 
 
-### VARIABLES ###
+##### VARIABLES #####
 
 # load enviroment variables from .env file
 load_dotenv()
 
 # get discord token from enviroment system
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+DISCORD_TOKEN = getenv("DISCORD_TOKEN")
 
 # set discord client with commands prefix
 client = commands.Bot(command_prefix='.')
 
 
-### FUNNCTIONS ###
+##### FUNNCTIONS #####
 @client.event
 async def on_ready():
     print(f"\a*We are already online, connected as {client.user.name}!*")
@@ -24,44 +24,36 @@ async def on_ready():
 
 @client.command()
 async def ping(ctx):
-    await ctx.send("Pong!")
+    await ctx.send("PONG!")
 
 
 @client.command(name="d")
 async def dice_roll(ctx, ring, skill):
-    converted = customdice.convert(ring=ring, skill=skill)
-    dicepool = customdice.dice_roller(
-        d6=converted["ring"], d12=converted["skill"])
-
-    result = customdice.format(ring=dicepool["d6"], skill=dicepool["d12"])
-    result = f"Ring Dice: {result['ring']}\nSkill Dice: {result['skill']}"
+    dice = CustomDice(ring=ring, skill=skill)
+    result = dice.format(**dice.roll())
 
     await ctx.send(result)
 
 
 @client.command(name="r")
 async def ring_roll(ctx, ring):
-    converted = customdice.convert(ring=ring)
-    dicepool = customdice.dice_roller(d6=converted["ring"])
-
-    result = customdice.format(ring=dicepool["d6"])
-    result = f"Ring Dice: {result['ring']}"
+    dice = CustomDice(ring=ring, skill=0)
+    result = dice.format(**dice.roll())
+    result = result.splitlines()[0]
 
     await ctx.send(result)
 
 
 @client.command(name="s")
 async def skill_roll(ctx, skill):
-    converted = customdice.convert(skill=skill)
-    dicepool = customdice.dice_roller(d12=converted["skill"])
-
-    result = customdice.format(skill=dicepool["d12"])
-    result = f"Skill Dice: {result['skill']}"
+    dice = CustomDice(ring=0, skill=skill)
+    result = dice.format(**dice.roll())
+    result = result.splitlines()[1]
 
     await ctx.send(result)
 
 
-### MAIN ###
+##### MAIN #####
 
 # connect
 client.run(DISCORD_TOKEN)
--- src/models/customdice.py
+++ src/models/customdice.py
@@ -1,53 +1,54 @@
 from random import randrange
 
 
-### VARIABLES ###
-dice = {
-    1: {"d6": "◻", "d12": "◻"},
-    2: {"d6": "🤔😫", "d12": "◻"},
-    3: {"d6": "🤔", "d12": "🤔"},
-    4: {"d6": "👏😫", "d12": "🤔"},
-    5: {"d6": "👏", "d12": "🤔"},
-    6: {"d6": "🎆😫", "d12": "👏😫"},
-    7: {"d12": "👏😫"},
-    8: {"d12": "👏"},
-    9: {"d12": "👏"},
-    10: {"d12": "👏🤔"},
-    11: {"d12": "🎆😫"},
-    12: {"d12": "🎆"}
-}
-
-
-### FUNCTIONS ###
-def convert(**kwargs):
-
-    try:
-        [kwargs.update({key: int(value)}) for key, value in kwargs.items()]
-
-    except ValueError:
-        print("Um dos argumentos não é válido")
-
-    else:
-        return kwargs
-
-
-def dice_roller(**kwargs):
-    results = {
-        "d6": [],
-        "d12": []
+##### CLASSES #####
+class CustomDice:
+    dice = {
+        1: {"d6": "◻", "d12": "◻"},
+        2: {"d6": "🤔😫", "d12": "◻"},
+        3: {"d6": "🤔", "d12": "🤔"},
+        4: {"d6": "👏😫", "d12": "🤔"},
+        5: {"d6": "👏", "d12": "🤔"},
+        6: {"d6": "🎆😫", "d12": "👏😫"},
+        7: {"d12": "👏😫"},
+        8: {"d12": "👏"},
+        9: {"d12": "👏"},
+        10: {"d12": "👏🤔"},
+        11: {"d12": "🎆😫"},
+        12: {"d12": "🎆"}
     }
 
-    for key, value in kwargs.items():
-        for i in range(1, value+1):
-            face = randrange(1, 7) if key == "d6" else randrange(1, 13)
-            results[key].append(dice[face][key])
+    def __init__(self, ring: int, skill: int):
+        self.ring = int(ring)
+        self.skill = int(skill)
 
-    return results
+    def roll(self):
+        values = {
+            "ring": self.ring,
+            "skill": self.skill
+        }
 
+        results = {
+            "ring": [],
+            "skill": []
+        }
 
-def format(**kwargs):
-    [kwargs.update({key: [f"[{face}]" for face in value]})
-     for key, value in kwargs.items()]
-    [kwargs.update({key: ", ".join(value)}) for key, value in kwargs.items()]
-    
-    return kwargs
+        for key, value in values.items():
+            for i in range(1, value+1):
+                dieface = randrange(
+                    1, 7) if key == "ring" else randrange(1, 13)
+                diekey = "d6" if key == "ring" else "d12"
+                results[key].append(self.dice[dieface][diekey])
+
+        return results
+
+    def format(self, **kwargs):
+        rolled_dice = kwargs
+
+        [rolled_dice.update({key: [f"[{face}]" for face in value]})
+         for key, value in rolled_dice.items()]
+        [rolled_dice.update({key: ', '.join(value)})
+         for key, value in rolled_dice.items()]
+        rolled_dice = f"Ring dice: {rolled_dice['ring']}\nSkill dice: {rolled_dice['skill']}"
+
+        return rolled_dice
